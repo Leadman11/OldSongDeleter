40334a92727a081e8a00ffd1b991320b5f788f0f
Chris He <hetianpeichris199@gmail.com>
2022-01-15 03:09:43 -0800

    initial commit

---
+++ src/OldSongDeleter/OldSongDeleter.java
@@ -0,0 +1,14 @@
+package OldSongDeleter;
+
+import java.io.File;
+
+
+public class OldSongDeleter {
+    public static final File CWD = new File(System.getProperty("user.dir"));
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
